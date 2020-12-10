4f72831fd7a084a97592955786dc07eba4c0b47b
Sable Seyler <sable@seyleri.us>
2020-12-09 21:28:48 -0600

    Add initial project stub

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject halp "0.1.0-SNAPSHOT"
+  :description "A Discord halper written in Clojure"
+  :url "https://github.com/seylerius/halp"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.suskalo/discljord "1.2.0"]]
+  :main ^:skip-aot halp.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}}
+  :repl-options {:init-ns 'halp.core})
