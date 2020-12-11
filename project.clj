d869b89a3448da9cac3e93a0520e3394bac95e0f
Sable Seyler <sable@seyleri.us>
2020-12-11 04:57:13 -0600

    Add logging and fix namespace initialization

--- project.clj
+++ project.clj
@@ -4,8 +4,11 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.suskalo/discljord "1.2.0"]]
+                 [org.suskalo/discljord "1.2.0"]
+                 [ch.qos.logback/logback-classic "1.2.3"]]
+  :jvm-opts ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/slf4j-factory"]
   :main ^:skip-aot halp.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
-  :repl-options {:init-ns 'halp.core})
+  :plugins [[lein-cljfmt "0.7.0"]]
+  :repl-options {:init-ns halp.core})
