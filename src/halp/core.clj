ef1f3cea2825acf45695dad1b3f72b4a69860c5f
Sable Seyler <sable@seyleri.us>
2020-12-11 04:57:57 -0600

    Add require specs and stub out main

--- src/halp/core.clj
+++ src/halp/core.clj
@@ -1,7 +1,8 @@
 (ns halp.core
   (:require [clojure.core.async :as a]
             [discljord.connections :as c]
-            [discljord.messaging :as m])
+            [discljord.messaging :as m]
+            [clojure.spec.alpha :as s])
   (:gen-class))
 
 (def token (System/getenv "DISCORD_AUTH_TOKEN"))
@@ -9,4 +10,8 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [event-ch (a/chan 100)
+        connection-ch (c/connect-bot! token event-ch
+                                      :intents #{:guild-messages
+                                                 :guild_message_reactions})
+        message-ch (m/start-connection! token)]))
